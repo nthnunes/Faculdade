f045b8c77a64e4354da16a72c4dac00db2c5d486
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-04-20 14:42:15 -0300

    Att

--- Estrutura de Dados/selectionSort.py
+++ Estrutura de Dados/selectionSort.py
@@ -8,8 +8,9 @@ def selectionSort(list, size):
         for i in range(step + 1, size):
             if list[i] < list[min]:
                 min = i
+        list[step], list[min] = list[min], list[step]
 
-        (list[step], list[min]) = (list[min], list[step])
+        
 
 randomList = random.sample(range(1, 50), 25)
 print(randomList)
