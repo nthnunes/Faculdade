46456f655a1051f46447fe551f710ff1abea831a
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-06-02 19:48:35 -0300

    Update inClass.py

--- Estrutura de Dados/inClass.py
+++ Estrutura de Dados/inClass.py
@@ -1,41 +1,20 @@
-def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
-  
-    # See if left child of root exists and is
-    # greater than root
-    if l < n and arr[i] < arr[l]:
-        largest = l
-  
-    # See if right child of root exists and is
-    # greater than root
-    if r < n and arr[largest] < arr[r]:
-        largest = r
-  
-    # Change root, if needed
-    if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i]  # swap
-  
-        # Heapify the root.
-        heapify(arr, n, largest)
-  
-# The main function to sort an array of given size
-def heapSort(arr):
-    n = len(arr)
-  
-    # Build a maxheap.
-    # Since last parent will be at ((n//2)-1) we can start at that location.
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-  
-    # One by one extract elements
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]   # swap
-        heapify(arr, i, 0)
-  
-# Driver code to test above
-arr = [ 12, 11, 13, 5, 6, 7]
-heapSort(arr)
-n = len(arr)
-print(arr)
+graph = {
+    '8' : ['4', '12'],
+    '4' : ['2', '6'],
+    '6' : ['5'],
+    '2' : ['1', '3'],
+    '12' : ['10', '14'],
+    '14' : ['13', '15'],
+    '10' : ['11']
+}
+
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+dfs(visited, graph, '14')
